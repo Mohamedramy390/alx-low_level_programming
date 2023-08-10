cd237f5f61065c0957dd693ff921e47579763d37
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-08-10 15:38:38 -0700

    size4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,9 @@
  */
 int main(void)
 {
-printf ("sizeof int : %zu byte(s)\n", sizeof(int));
-printf ("sizeof char : %zu byte(s)\n", sizeof(char));
-printf ("sizeof float : %zu byte(s)\n", sizeof(float));
-printf ("sizeof long int : %zu byte(s)\n", sizeof(long int));
-printf ("sizeof long long int : %zu byte(s)\n", sizeof(long long int));
+printf ("Size of a char : %zu byte(s)\n", sizeof(char));
+printf ("Size of an int : %zu byte(s)\n", sizeof(int));
+printf ("Size of a long int : %zu byte(s)\n", sizeof(long int));
+printf ("Size of a long long int : %zu byte(s)\n", sizeof(long long int));
+printf ("Size of a float : %zu byte(s)\n", sizeof(float));
 }
