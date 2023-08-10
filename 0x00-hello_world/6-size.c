ea3400fd7c55016b55d61a0d46e5e81362b426fd
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-08-10 15:34:50 -0700

    size3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,6 @@
 int main(void)
 {
 printf ("sizeof int : %zu byte(s)\n", sizeof(int));
-printf ("sizeof long : %zu byte(s)\n", sizeof(long));
 printf ("sizeof char : %zu byte(s)\n", sizeof(char));
 printf ("sizeof float : %zu byte(s)\n", sizeof(float));
 printf ("sizeof long int : %zu byte(s)\n", sizeof(long int));
