9d07a4c6430c5842fa63ca5a901fef5777b7f8c7
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-08-10 09:52:42 -0700

    bella

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
- * main prints the value in puts
+/**
+ * main - prints the value in puts
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
-}
+};
