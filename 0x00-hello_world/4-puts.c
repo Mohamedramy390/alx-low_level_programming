d9ef83ce8d06c90a8da27f6359e2c3dcae811fe7
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-08-10 09:34:37 -0700

    kman

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main(void){
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n\"");
 		return 0;
 }
+/*
+ * main prints the value in puts
+ */
