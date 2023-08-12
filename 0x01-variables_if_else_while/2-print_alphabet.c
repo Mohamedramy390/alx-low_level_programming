07df7fe7e1de8b0db4ddb07b80ab185b83f111aa
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-08-12 10:42:15 -0700

    alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphapet small
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num + 97 < 122)
+	{
+		putchar(num + 97);
+		num++;
+	}
+return (0);
+}
