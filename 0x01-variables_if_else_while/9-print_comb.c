903229206089859b0a47d24941838873d3086e9c
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-08-14 08:57:33 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int n = 0;
+
+while (n + 48 <= 57)
+{
+putchar(n + 48);
+if (n + 48 == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+n++;
+}
+putchar('\n');
+return (0);
