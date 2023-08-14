1e02919eabaca522c0fb9276acc7556e0e3061e4
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-08-14 07:59:58 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int n = 0;
+
+while (n < 10)
+{
+putchar(n + 48);
+n++;
+}
+putchar("\n");
+return (0);
+}
