700270e517ef642876c6942c8896f630bca94561
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-08-14 08:11:49 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints
+ * Return: 0 (succsess)
+ */
+int main(void)
+{
+int n = 0;
+
+while (n + 122 <= 122)
+{
+putchar(n + 122);
+n--;
+if (n + 122 == 96)
+{
+break;
+}
+}
+putchar('\n');
+return (0);
+}
