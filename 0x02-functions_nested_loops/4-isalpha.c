075e675713da621f3504114f4a303d33ba07cc69
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-08-16 15:59:12 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - prints
+ * @c: chracter to be checked
+ * Return: 1 for lowercase / Return: 0 other
+ */
+int _isalpha(int c)
+{
+if (c >= 97 && c <= 122 && c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
+
