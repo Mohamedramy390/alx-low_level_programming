de2ef3d2626450f37f1639e66f657ae5408543d5
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-08-16 15:34:21 -0700

    alphax10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include main.h
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int n;
+
+for (n = 1; n <= 10; n++)
+{
+for (c = 'a'; c <= 'z' ; c++)
+{
+putchar(c);
+}
+putchar('\n');
+}
+}
