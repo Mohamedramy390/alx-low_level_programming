b014e39fe50695573819f2cb944154cc2d06a4c5
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-08-24 19:55:36 -0700

    all2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,23 @@
  *
  * Return: dest
  */
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int c,i;
+	int i;
+	int j;
 
-	c=0;
-	while (dest[c])
-		c++;
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i] = src[i];
-	dest[c + i] = '\0';
-return {dest};	
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
