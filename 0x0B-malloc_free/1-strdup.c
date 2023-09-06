e1fb745f78e9451c5704dacdec694d9404c70806
mohamed ramy <mohamed.ramy20052014@gmail.com>
2023-09-06 05:48:35 -0700

    copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+char *_strdup(char *str)
+{
+	int size = 0, i = 0;
+	char* m;
+
+	if (str == 0)
+		return (NULL);
+
+	for (; str[size] =! '\0'; size++);
+
+	m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
+	{
+		return (NULL)
+	} else 
+	{
+		for (i; i < size; i++)
+			m[]i = str[i];
+	}
+	return (m);
+}
